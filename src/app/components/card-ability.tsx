2d66c946f2d026ab914f4aba9390029c33e4ef41
Paulo <paulorfsantos17@gmail.com>
2024-05-22 09:09:40 -0300

    feat: add CardAbility

---
+++ src/app/components/card-ability.tsx
@@ -0,0 +1,9 @@
+import { FaHtml5 } from 'react-icons/fa'
+
+export default function CardAbility() {
+  return (
+    <div className=" flex  min-h-20 min-w-20 max-w-36 items-center justify-center rounded-lg border-2 border-primary-light md:h-36">
+      <FaHtml5 className="h-2/3 w-2/3 text-primary-light" />
+    </div>
+  )
+}
