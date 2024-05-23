3305458b9ac346e0e437010bfed55e3992645720
Paulo <paulorfsantos17@gmail.com>
2024-05-23 13:29:30 -0300

    feat: add animation projects

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -10,7 +10,11 @@ import SocialButton from './social-button'
 export default function About() {
   return (
     <main className="mx-8 my-24 mb-12 flex flex-col items-center gap-12 bg-particles-mobile sm:mx-12 sm:grid sm:grid-cols-2">
-      <ScrollAnimation animateIn="animate__fadeInLeft" duration={1}>
+      <ScrollAnimation
+        animateIn="animate__fadeInLeft"
+        duration={1}
+        offset={200}
+      >
         <div className=" flex flex-col gap-12 sm:gap-8">
           <h1
             className="inline-block bg-gradient-to-r from-secondary-light via-primary-light to-primary 
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from 'react'
+import ScrollAnimation from 'react-animate-on-scroll'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 import CardProject from './card-project'
@@ -31,40 +32,44 @@ export default function Projects() {
 
   return (
     <section className="my-8 ml-8 space-y-6 sm:mx-10">
-      <h2
-        className="font-ultralife text-3xl
+      <ScrollAnimation animateIn="animate__fadeInLeft" offset={300}>
+        <h2
+          className="font-ultralife text-3xl
           uppercase text-slate-200"
-      >
-        Projetos
-      </h2>
-      <div className="flex  min-h-[450px] items-center gap-8 ">
-        <button
-          onClick={() => handleScrollLeft()}
-          onMouseOver={() => handleScrollLeft()}
-          className="hidden sm:block "
         >
-          <FiChevronLeft className="h-8 w-8 rounded-full font-bold  text-primary-light ease-in hover:bg-primary-light hover:text-slate-200 hover:duration-300" />
-        </button>
-        <div
-          id="slider"
-          className=" flex h-[450px] items-center gap-12 overflow-x-auto px-4 [&::-webkit-scrollbar]:hidden "
-        >
-          <CardProject />
-          <CardProject />
-          <CardProject />
-          <CardProject />
-          <CardProject />
-          <CardProject />
-          <CardProject />
+          Projetos
+        </h2>
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="animate__fadeInRight" delay={1000}>
+        <div className="flex  min-h-[450px] items-center gap-8 ">
+          <button
+            onClick={() => handleScrollLeft()}
+            onMouseOver={() => handleScrollLeft()}
+            className="hidden sm:block "
+          >
+            <FiChevronLeft className="h-8 w-8 rounded-full font-bold  text-primary-light ease-in hover:bg-primary-light hover:text-slate-200 hover:duration-300" />
+          </button>
+          <div
+            id="slider"
+            className=" flex h-[450px] items-center gap-12 overflow-x-auto px-4 [&::-webkit-scrollbar]:hidden "
+          >
+            <CardProject />
+            <CardProject />
+            <CardProject />
+            <CardProject />
+            <CardProject />
+            <CardProject />
+            <CardProject />
+          </div>
+          <button
+            className="hidden sm:block"
+            onMouseOver={() => handleScrollRight()}
+            onClick={() => handleScrollRight()}
+          >
+            <FiChevronRight className="h-8 w-8 rounded-full font-bold  text-primary-light ease-in hover:bg-primary-light hover:text-slate-200 hover:duration-300" />
+          </button>
         </div>
-        <button
-          className="hidden sm:block"
-          onMouseOver={() => handleScrollRight()}
-          onClick={() => handleScrollRight()}
-        >
-          <FiChevronRight className="h-8 w-8 rounded-full font-bold  text-primary-light ease-in hover:bg-primary-light hover:text-slate-200 hover:duration-300" />
-        </button>
-      </div>
+      </ScrollAnimation>
     </section>
   )
 }
