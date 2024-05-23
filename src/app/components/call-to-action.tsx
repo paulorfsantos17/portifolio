10814d6caf8cd34bde03e2ea24557f99c36621eb
Paulo <paulorfsantos17@gmail.com>
2024-05-23 17:07:34 -0300

    fix: button call-to-action animate

--- src/app/components/call-to-action.tsx
+++ src/app/components/call-to-action.tsx
@@ -7,6 +7,7 @@ export default function CallToAction() {
         justify-center gap-16 bg-gradient-to-r  from-secondary-light via-primary-light to-primary 
         bg-clip-text 
         font-ultralife text-transparent md:mt-40 md:px-36"
+      data-taos-offset="600"
     >
       <h2 className="text-center  font-ultralife text-3xl font-bold uppercase md:text-5xl  ">
         Interessado em colaborar comigo?
@@ -17,13 +18,13 @@ export default function CallToAction() {
       </p>
       <button
         className="group  relative z-10 flex items-center gap-4 rounded-full border border-transparent bg-primary
-          px-6 py-3 font-ultralife font-bold uppercase text-slate-200 duration-300
-          ease-in hover:border-primary-light hover:bg-transparent
-        hover:text-primary-light"
+        px-6 py-3 font-ultralife font-bold uppercase text-slate-200 duration-300
+        ease-in hover:border-primary-light hover:bg-transparent
+      hover:text-primary-light"
       >
         <div
           className=" z-5 absolute right-[-1px] box-content  h-[80%] w-[100%] scale-50 animate-ping rounded-full
-            bg-primary opacity-100 blur-sm duration-75 ease-in group-hover:animate-none group-hover:opacity-0"
+            bg-primary opacity-100 blur-sm   group-hover:animate-none group-hover:opacity-0"
         ></div>
         <PiChat className="z-10 h-8 w-8 " />
         <p className="z-10">Iniciar uma conversa</p>
