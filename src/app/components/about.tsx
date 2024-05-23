7cdac9ecd5496dff1ca405cb3adfe3592b1a1acd
Paulo <paulorfsantos17@gmail.com>
2024-05-23 11:53:50 -0300

    faet: add animation text

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Image from 'next/image'
+import ScrollAnimation from 'react-animate-on-scroll'
 import { FaGithub, FaLinkedinIn, FaWhatsapp } from 'react-icons/fa'
 
 import Avatar from '@/app/assets/avatar.png'
@@ -7,25 +10,25 @@ import SocialButton from './social-button'
 export default function About() {
   return (
     <main className="mx-8 my-24 mb-12 flex flex-col items-center gap-12 bg-particles-mobile sm:mx-12 sm:grid sm:grid-cols-2">
-      <div
-        className=" flex flex-col
-          gap-12 sm:gap-8"
-      >
-        <h1
-          className="inline-block bg-gradient-to-r from-secondary-light via-primary-light to-primary 
+      <ScrollAnimation animateIn="animate__fadeInRight">
+        <div className=" flex flex-col gap-12 sm:gap-8">
+          <h1
+            className="inline-block bg-gradient-to-r from-secondary-light via-primary-light to-primary 
           bg-clip-text text-center font-ultralife text-4xl 
           font-bold uppercase text-transparent sm:text-left sm:text-6xl"
-        >
-          Desenvolvedor Web
-        </h1>
+          >
+            Desenvolvedor Web
+          </h1>
 
-        <p className="text-slate-200">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non
-          feugiat eros. Nam malesuada placerat mi, vel finibus eros malesuada
-          et. Duis ultrices sit amet orci non congue. Cras pellentesque lectus
-          ac magna vulputate, quis convallis nisi lobortis.
-        </p>
-      </div>
+          <p className="text-slate-200">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            non feugiat eros. Nam malesuada placerat mi, vel finibus eros
+            malesuada et. Duis ultrices sit amet orci non congue. Cras
+            pellentesque lectus ac magna vulputate, quis convallis nisi
+            lobortis.
+          </p>
+        </div>
+      </ScrollAnimation>
 
       <div className="flex flex-col items-center justify-center gap-8 ">
         <Image
