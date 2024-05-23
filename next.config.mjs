95e5e67332e8106119bfa5298e0474cbf51e6dc9
Paulo <paulorfsantos17@gmail.com>
2024-05-23 14:38:47 -0300

    feat: add unsure css

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+import withCss from '@zeit/next-css'
+import withPurgeCss from 'next-purgecss'
+
+const nextConfig = {
+  withCss: withCss(
+    withPurgeCss({
+      purgeCss: {
+        paths: ['./app/**/*.{js,ts,jsx,tsx}'],
+        extractors: [
+          {
+            extractor: class {
+              static extract(content) {
+                return content.match(/[A-Za-z0-9-_:/]+/g) || []
+              }
+            },
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
+          },
+        ],
+      },
+    }),
+  ),
+}
 
 export default nextConfig
