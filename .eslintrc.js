42377e4983b80c88739ac90659ffe56ae8a0073f
Paulo <paulorfsantos17@gmail.com>
2024-05-23 14:15:47 -0300

    feat: config next/web-vitals

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,10 @@ module.exports = {
     sourceType: 'module',
     ecmaVersion: 'latest',
   },
-  extends: ['@rocketseat/eslint-config/next'],
+  extends: [
+    'eslint:recommended',
+    'next',
+    '@rocketseat/eslint-config/next',
+    'next/core-web-vitals',
+  ],
 }
