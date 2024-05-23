6107a19099a11477a27017e1261c9ab994620494
Paulo <paulorfsantos17@gmail.com>
2024-05-23 12:12:44 -0300

    feat: add animation about

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -10,7 +10,7 @@ import SocialButton from './social-button'
 export default function About() {
   return (
     <main className="mx-8 my-24 mb-12 flex flex-col items-center gap-12 bg-particles-mobile sm:mx-12 sm:grid sm:grid-cols-2">
-      <ScrollAnimation animateIn="animate__fadeInRight" duration={0.5}>
+      <ScrollAnimation animateIn="animate__fadeInLeft" duration={1}>
         <div className=" flex flex-col gap-12 sm:gap-8">
           <h1
             className="inline-block bg-gradient-to-r from-secondary-light via-primary-light to-primary 
@@ -31,27 +31,30 @@ export default function About() {
       </ScrollAnimation>
 
       <div className="flex flex-col items-center justify-center gap-8 ">
-        <Image
-          src={Avatar}
-          alt=""
-          width={260}
-          className="h-max-[260] sm:max-w-[180px]"
-        />
-
-        <nav className="flex h-20 w-full justify-center gap-16">
-          <SocialButton
-            icon={<FaWhatsapp className="h-8 w-8" />}
-            title="Whatsapp"
+        <ScrollAnimation animateIn="animate__fadeInUp" offset={400}>
+          <Image
+            src={Avatar}
+            alt=""
+            width={260}
+            className="h-max-[260] sm:max-w-[180px]"
           />
-          <SocialButton
-            icon={<FaLinkedinIn className="h-8 w-8" />}
-            title="Linkedin"
-          />
-          <SocialButton
-            icon={<FaGithub className="h-8 w-8" />}
-            title="GitHub"
-          />
-        </nav>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="animate__fadeInDown">
+          <nav className="flex h-20 w-full justify-center gap-16">
+            <SocialButton
+              icon={<FaWhatsapp className="h-8 w-8" />}
+              title="Whatsapp"
+            />
+            <SocialButton
+              icon={<FaLinkedinIn className="h-8 w-8" />}
+              title="Linkedin"
+            />
+            <SocialButton
+              icon={<FaGithub className="h-8 w-8" />}
+              title="GitHub"
+            />
+          </nav>
+        </ScrollAnimation>
       </div>
     </main>
   )
