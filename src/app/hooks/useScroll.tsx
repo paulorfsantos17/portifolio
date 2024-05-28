44db13884367b62b332a646cc90b72ad0805f33d
Paulo <paulorfsantos17@gmail.com>
2024-05-28 16:08:07 -0300

    fix: error windows undefined

--- src/app/hooks/useScroll.tsx
+++ src/app/hooks/useScroll.tsx
@@ -1,23 +1,23 @@
 import { useEffect, useState } from 'react'
 
 export function useScroll() {
-  // Armazena o valor do scroll
-  const [scrollPosition, setScrollPosition] = useState(window.scrollY)
+  const [scrollPosition, setScrollPosition] = useState(0)
 
   useEffect(() => {
-    // Função para atualizar o valor do scroll
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY)
-    }
+    if (typeof window !== 'undefined') {
+      const handleScroll = () => {
+        setScrollPosition(window.scrollY)
+      }
 
-    // Adiciona o evento de scroll ao window
-    window.addEventListener('scroll', handleScroll)
+      window.addEventListener('scroll', handleScroll)
 
-    // Limpa o evento ao desmontar
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
+      return () => {
+        window.removeEventListener('scroll', handleScroll)
+      }
     }
   }, [])
 
-  return { scrollPosition }
+  return {
+    scrollPosition,
+  }
 }
